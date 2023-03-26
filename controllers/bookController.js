4bca452d0b796c0eba69f18fd348681db66ac3f7
pragya-gupta7 <20je0695@ese.iitism.ac.in>
2023-03-26 14:51:22 +0530

    adding home page

---
+++ controllers/bookController.js
@@ -0,0 +1,75 @@
+const Book = require("../models/book");
+const Author = require("../models/author");
+const Genre = require("../models/genre");
+const BookInstance = require("../models/bookinstance");
+
+const async = require("async");
+
+exports.index = (req, res) => {
+  async.parallel(
+    {
+      book_count(callback) {
+        Book.countDocuments({}, callback);
+      },
+      book_instance_count(callback) {
+        BookInstance.countDocuments({}, callback);
+      },
+      book_instance_available_count(callback) {
+        BookInstance.countDocuments({ status: "Available" }, callback);
+      },
+      author_count(callback) {
+        Author.countDocuments({}, callback);
+      },
+      genre_count(callback) {
+        Genre.countDocuments({}, callback);
+      },
+    },
+    (err, results) => {
+      res.render("index", {
+        title: "Local Library Home",
+        error: err,
+        data: results,
+      });
+    }
+  );
+};
+
+// Display list of all books.
+exports.book_list = (req, res) => {
+  res.send("NOT IMPLEMENTED: Book list");
+};
+
+// Display detail page for a specific book.
+exports.book_detail = (req, res) => {
+  res.send(`NOT IMPLEMENTED: Book detail: ${req.params.id}`);
+};
+
+// Display book create form on GET.
+exports.book_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Book create GET");
+};
+
+// Handle book create on POST.
+exports.book_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Book create POST");
+};
+
+// Display book delete form on GET.
+exports.book_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Book delete GET");
+};
+
+// Handle book delete on POST.
+exports.book_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Book delete POST");
+};
+
+// Display book update form on GET.
+exports.book_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Book update GET");
+};
+
+// Handle book update on POST.
+exports.book_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Book update POST");
+};
